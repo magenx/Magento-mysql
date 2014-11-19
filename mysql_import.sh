9981f7a56938cc92ecfc06f3fd3381390f845e38
magenx <designelo@gmail.com>
2014-11-19 15:34:28 +0000

    Create mysql_import.sh

---
+++ mysql_import.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+### MySQL Setup ###
+MUSER="xxxxxx"
+MPASS="xxxxxx"
+MHOST="xxxxxx"
+MDB="xxxxxx"
+
+### Action ###
+read -p "---> Enter MySQL backup gz file name : " MYSQL_BKP_FILE
+
+if [ -e "${MYSQL_BKP_FILE}" ]; then
+    echo "Database import"
+      zcat ${MYSQL_BKP_FILE} | mysql -f -u ${MUSER} -h ${MHOST} -p${MPASS} ${MDB}
+    echo
+	echo "Checking MySQL"
+    echo
+      mysqlcheck -u ${MUSER} -h ${MHOST} -p${MPASS} ${MDB}
+    echo
+	echo "All done"
+else 
+    echo "File: ${MYSQL_BKP_FILE} does not exist"
+	exit 1
+fi
