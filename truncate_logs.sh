1f67fdbe739938f2f46efc1f3009fd64703af840
magenx <designelo@gmail.com>
2015-11-03 21:43:51 +0000

    Update truncate_logs.sh

--- truncate_logs.sh
+++ truncate_logs.sh
@@ -11,7 +11,7 @@ DB_HOST=""
 DB_USER_NAME=""
 DB_PASS=""
 DB_NAME=""
-TABLES="log_url log_url_info log_visitor log_visitor_info"
+TABLES="catalog_compare_item dataflow_batch_export dataflow_batch_import log_customer log_visitor log_visitor_info log_url log_url_info log_quote report_viewed_product_index report_compared_product_index report_event"
 
 for table in ${TABLES}
 do
