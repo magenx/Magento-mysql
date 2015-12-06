91520e550b7bb3f700d4246c2450f0de3793def8
magenx <designelo@gmail.com>
2015-12-06 08:31:54 +0000

    Create files_transfer.sh

---
+++ files_transfer.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+### TRANSFER DATABASE AND FILES FROM REMOTE
+
+TIME=$(date +"%H-%M-%S")
+DAY=$(date +"%d-%m-%Y")
+FILE=mysql-transfer.${DAY}.${TIME}.sql.gz
+OPTIONS="--single-transaction --routines --triggers --events"
+MYSQLDATABASE=""
+MYSQLUSER=""
+MYSQLPASS=""
+MYSQLHOST=""
+SERVERIP=""
+REMOTEUSER=""
+LOCALUSER=""
+REMOTEPATH="/var/www/html/magento/"
+LOCALPATH="/var/www/html/magento/"
+EXCLUDEFROM="/root/exclude.txt"
+
+echo "RSYNC FILES FROM REMOTE SERVER"
+rsync -vrltpD --exclude-from=${EXCLUDEFROM} ${REMOTEUSER}@${SERVERIP}:${REMOTEPATH} ${LOCALPATH}
+chown -R ${LOCALUSER}:${LOCALUSER} ${LOCALPATH}
+echo "<<<   ----   done   ----   >>>"
+
+echo "MYSQLDUMP ON REMOTE SERVER"
+ssh ${REMOTEUSER}@${SERVERIP} "mysqldump -u ${MYSQLUSER} -h ${MYSQLHOST} -p${MYSQLPASS} ${OPTIONS} ${MYSQLDATABASE} | gzip > ${REMOTEPATH}${FILE}"
+
+rsync -avz --remove-source-files ${REMOTEUSER}@${SERVERIP}:${REMOTEPATH}${FILE} .
+
+echo "DATABASE IMPORT. PLEASE WAIT."
+zcat ${FILE} | mysql -f ${MYSQLDATABASE}
+
+mysqlcheck ${MYSQLDATABASE}
+
+echo
+echo "<<<   ----   done   ----   >>>"
